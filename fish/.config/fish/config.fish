982bcf42c171176f7ca3824c20fda2f5e369acf4
Sergey Metlenkin <sergei.metlenkin@mcc-tomsk.de>
2021-10-22 16:09:35 +0700

    Added fish config

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set fish_greeting
+
+rvm default
+
+set -gx ANDROID_HOME $HOME/Android/Sdk
+fish_add_path $ANDROID_HOME/emulator
+fish_add_path $ANDROID_HOME/tools
+fish_add_path $ANDROID_HOME/tools/bin
+fish_add_path $ANDROID_HOME/platform-tools
