f464b2de4d7bd31057c7be3e43b4350dbd8d32aa
Sergey Metlenkin <sergei.metlenkin@mcc-tomsk.de>
2021-11-09 15:42:44 +0700

    Adjusted config files

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -11,3 +11,5 @@ fish_add_path $ANDROID_HOME/emulator
 fish_add_path $ANDROID_HOME/tools
 fish_add_path $ANDROID_HOME/tools/bin
 fish_add_path $ANDROID_HOME/platform-tools
+
+set -gx XDG_DATA_DIRS /var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share
